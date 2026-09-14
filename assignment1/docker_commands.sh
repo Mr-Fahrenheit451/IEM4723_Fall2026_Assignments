2428293ab4426f00ef600cf86e558977e14ac136
Mr-Fahrenheit451 <kailuagirl5210@gmail.com>
2026-09-14 18:10:32 -0500

    Appending all commands from question 1

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -1,1 +1,10 @@
+Assignment 1: Docker Commands
+
+Question 1
+cd C:\Users\kailu\OneDrive\Documents\IEM4723\IEM4723_Fall2026_Assignments\assignment1\regression_container
+docker build -t linear-regression-app .
+docker tag linear-regression-app linear-regression-app:v0.1.1
+docker tag linear-regression-app linear-regression-app:v0.1.2
+docker tag linear-regression-app mrfahrenheit451/linear-regression-app:latest
+docker push mrfahrenheit451/linear-regression-app:latest
 
