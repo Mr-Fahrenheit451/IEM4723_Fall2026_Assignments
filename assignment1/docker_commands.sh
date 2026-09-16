fda71b954dffa2c380fbda89f80c346c796e1bcb
Mr-Fahrenheit451 <kailuagirl5210@gmail.com>
2026-09-15 22:08:14 -0500

    Appended commands for Q3

--- assignment1/docker_commands.sh
+++ assignment1/docker_commands.sh
@@ -17,3 +17,12 @@ docker run --name regression-ctnr --volume ".\data:/home/iem4723/data" regressio
 cd C:\Users\kailu\OneDrive\Documents\IEM4723\IEM4723_Fall2026_Assignments\assignment1\layering
 docker build -t logistic_regression_layered .
 docker run --name layered-cntr --volume "C:\Users\kailu\OneDrive\Documents\IEM4723\IEM4723_Fall2026_Assignments\assignment1\layering\data:/home/iem4723/data" logistic_regression_layered:latest
+
+# Question 4
+cd C:\Users\kailu\OneDrive\Documents\IEM4723\IEM4723_Fall2026_Assignments\assignment1\port_listening
+docker build -t question3-app .
+docker run --name question3-app --publish 3333:8000 question3-app:latest
+
+(Then docker stop question3-app and docker rm question3-app)
+
+This works! I uploaded screenshot verification to the README along with the commit picture.
